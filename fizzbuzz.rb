2e0d01005aef18ba2238551ac2a5189ad7c73b6d
Atul Tiwari <atul.tiwari@krishnacollege.ac.in>
2020-06-01 11:13:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(number)
 # if((number%3==0) && (number%5==0))
   #  return "FizzBuzz"
- if(number%3==0)
+ if(number%3 == 0)
     "Fizz"
-  elsif(number%5==0)
+  elsif(number%5 == 0)
     "Buzz"
   else
     "nil"
